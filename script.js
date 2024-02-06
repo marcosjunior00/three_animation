f656ca81ef441ce4cf0e202d01010bd307c1c9e3
Marcos Junior <132008242+marcosjunior00@users.noreply.github.com>
2024-02-06 12:06:53 -0300

    Update var names

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.ConeGeometry( 1.5, 2.4, 22 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cone = new THREE.Mesh( geometry, material );
+scene.add( cone );
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	cone.rotation.x += 0.01;
+	cone.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
